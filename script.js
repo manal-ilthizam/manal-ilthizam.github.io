03e74028b1bbe2faa98f2d584ea985f2fd0f32d6
Manal Ilthizam <ilthizam.manaal@gmail.com>
2025-11-10 22:27:24 +0530

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll navigation
+document.querySelectorAll('nav a').forEach(a => {
+  a.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(a.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
